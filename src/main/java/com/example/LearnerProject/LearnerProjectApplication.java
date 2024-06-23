c334773b8cec61e3d82dabdbd3ad7e806800c735
ParvKukreja <gautamparv@gmail.com>
2024-06-23 13:50:16 +0530

    initial commit

---
+++ src/main/java/com/example/LearnerProject/LearnerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerProjectApplication.class, args);
+	}
+
+}
